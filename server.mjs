3f494e11ad600d8b3dbd0fb880e5fbeae08bffc4
RSarvjyoti <sarvjyoti64@gmail.com>
2024-08-19 00:13:19 +0530

    fixed

--- server.mjs
+++ server.mjs
@@ -9,13 +9,6 @@ import eventRoutes from "./routes/eventRoutes.mjs";
 import userRoutes from "./routes/userRoutes.mjs";
 import { requestLogger } from "./utils/logger.mjs";
 
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-  }));
-  
-
 config();
 
 const PORT = process.env.PORT || 600
@@ -24,8 +17,14 @@ const app = express();
 app.use(express.json());
 
 app.use(requestLogger);
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
+  
 
-app.use(cors());
+
 app.use(morgan('dev'));
 app.use('/uploads', express.static('uploads'));
 
